966afe1300f2cd4000f97191b6837e2ef45e0b35
yv <vasilchenko.yegor@gmail.com>
2016-11-06 12:02:39 +0200

    Added Login and Registration forms

--- bets/views.py
+++ bets/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.urls import reverse
 from django.views.generic import TemplateView
 
 from .models import Bet
