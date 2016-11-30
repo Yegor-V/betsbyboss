470dc638d1a746234955da39ec10a0f2d7ac1aac
yv <vasilchenko.yegor@gmail.com>
2016-11-30 23:14:54 +0200

    Added image to analytics, applied divs to analytics details

--- bets/models.py
+++ bets/models.py
@@ -9,13 +9,23 @@ class Analytics(models.Model):
     game_time = models.DateTimeField()
     is_live_bets = models.BooleanField()
     text = models.TextField()
+    image = models.ImageField()
 
     def __str__(self):
         return '{} vs {}'.format(self.team_1, self.team_2)
 
     @property
     def text_beginning(self):
-        return ' '.join(self.text.split(' ')[:10])
+        return ' '.join(self.text.split(' ')[:20]) + '...'
+
+    @property
+    def str_time(self):
+        date_time = str(self.game_time)
+        day = date_time.split('-')[2][0:2]
+        month = date_time.split('-')[1]
+        year = date_time.split('-')[0]
+        time = date_time.split(' ')[1][0:5]
+        return "{}/{}/{} {}".format(day, month, year, time)
 
 
 class Article(models.Model):
